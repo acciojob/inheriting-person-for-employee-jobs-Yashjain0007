b306d7c77ec8277ce477a8dddb327d2e6a15417a
Yashjain0007 <124593014+Yashjain0007@users.noreply.github.com>
2023-04-08 23:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
-	greet(){
-		console.log("Hello, my name is"+ this.name+", I am "+this.age+" years old.")
-	}
+function Person(name,age) {
+	this.name=name;
+	this.age=age;	
 }
-
-function Employee extends Person (name, age, jobTitle) {
-   super(name,age);
-	this.jobTitle = jobTitle;
-	jobGreet(){
-		console.log("Hello, my name is "+ this.name + ", I am "+ this.age + " years old, and my job title is" + this.jobTitle+".")
-	}
+Person.prototype.greet=function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 }
+function Employee(name,age,jobTitle){
+	Person.call(name,age);
+	this.jobTitle=jobTitle;
+}
+Employee.prototype=Object.create(Person.prototype);
+Employee.prototype.jobGreet=function (){
+	
+	console.log(`Hello, my name is ${this.nam}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+}
+window.Person=Person;
+window.Employee=Employee;
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+
